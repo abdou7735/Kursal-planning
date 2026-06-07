49f3a479c5f336aa0e2f1013453d1fd6824e407b
abdou7735 <77749476+abdou7735@users.noreply.github.com>
2026-06-07 11:20:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore, doc, collection, onSnapshot, setDoc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
